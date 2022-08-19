8c237566eb45ae25fe960f716128deb752ae6e6a
Jayden Griffith <griffithjayden97@outlook.com>
2022-08-19 17:55:00 -0600

    Added requests to controllers.

---
+++ controllers/thoughtController.js
@@ -0,0 +1,53 @@
+const { Thought } = require('../models');
+
+module.exports = {
+  // Get all thoughts
+  getThoughts(req, res) {
+    Thought.find()
+      .then((thoughts) => res.json(thoughts))
+      .catch((err) => res.status(500).json(err));
+  },
+  // Get a thought
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+    .select('-__v')
+    .then((thought) =>
+      !thought
+        ? res.status(404).json({ message: 'No thought under that ID' })
+        : res.json(thought)
+    )
+    .catch((err) => res.status(500).json(err));
+  },
+  // Create a thought
+  createThought(req, res) {
+    Thought.create(req.body)
+      .then((thought) => res.json(thought))
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+  // Delete a thought
+  deleteThought(req, res) {
+    Thought.findOneAndDelete({ _id: req.paramas.thoughtId })
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: 'No thought under that ID' })
+          : res.json({ message: 'Thought Deleted!' })
+      )
+  },
+  // Update a thought
+  updateThought(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+    .then((thought) => 
+      !thought
+        ? res.status(404).json({ message: 'No thought under that ID' })
+        : res.json(thought)
+    )
+    .catch((err) => res.status(500).json(err));
+  },
+};
---
+++ controllers/userController.js
@@ -0,0 +1,54 @@
+const { User, Thought } = require('../models');
+
+module.exports = {
+  // Get all users
+  getUsers(req, res) {
+    User.find()
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
+  // Get a user
+  getSingleUser(req, res) {
+    User.findOne()
+      .select('-__v')
+      .then((user) => 
+        !user
+          ? res.status(404).json({ message: 'No user under that ID' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  // Create a user
+  createUser(req, res) {
+    // Need to look into this one a bit, particularly regarding the email
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+  deleteUser(req, res) {
+    User.findOneAndDelete({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user under that ID' })
+          : Thought.deleteMany({ _id: { $in: user.thoughts } })
+      )
+      .then(() => res.json({ message: 'User and associated thoughts deleted!' }))
+      .catch((err) => res.status(500).json(err));
+  },
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+    .then((user) =>
+      !user
+        ? res.status(404).json({ message: 'No user under this ID' })
+        : res.json(user)
+    )
+    .catch((err) => res.status(500).json(err));
+  },
+};
