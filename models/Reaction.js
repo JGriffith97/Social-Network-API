243a37a94c955f0ce4695ac9ac6ccfcb9c22ad53
Jayden Griffith <griffithjayden97@outlook.com>
2022-08-19 16:44:42 -0600

    Added getter function to Reaction.js

--- models/Reaction.js
+++ models/Reaction.js
@@ -28,5 +28,19 @@ const reactionSchema = new Schema(
 ); 
 
 // Need a getter method to format the timestamp on query.
+reactionSchema
+  .get(function () {
+    return this.createdAt.toLocaleString();
+  })
+
+  // This is a note from a prior SQL project,
+  // just in case the above doesn't work.
+  // module.exports = {
+  //   format_date: (date) => {
+  //     let newDate = new Date(date)
+  //     // Format date as MM/DD/YYYY
+  //     return newDate.toLocaleDateString();
+  //   },
+  // };
 
 module.exports = reactionSchema;
